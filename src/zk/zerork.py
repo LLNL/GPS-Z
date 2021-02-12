bd9bf9702eab166ac3b76b90b3e364c9c6c9b3ca
Russell Whitesides <whitesides1@llnl.gov>
2021-02-12 08:59:36 -0800

    Trying to detangle a botched commmit/merge with zerork_pool

--- src/zk/zerork.py
+++ src/zk/zerork.py
@@ -55,7 +55,7 @@ def zerork(dir_desk, atm, T0, fuel_fracs, oxid_fracs, phi, species_names, rxn_eq
     for sp in species_names:
         if sp not in fuel_fracs and sp not in oxid_fracs:
             other_species.append(sp)
-    trace_fracs_str = ','.join([str(sp)+": 0" for sp in other_species])
+    trace_fracs_str = ',\n'.join([str(sp)+": 0" for sp in other_species])
 
     #Write zero-rk input file
     error_return = False
@@ -78,6 +78,8 @@ def zerork(dir_desk, atm, T0, fuel_fracs, oxid_fracs, phi, species_names, rxn_eq
                 TRACE_FRACS=trace_fracs_str))
 
         zerork_out=''
+        env = dict(os.environ)
+        env["ZERORK_SPLIT_REVERSIBLE_REACTIONS"] = str(1)
         try:
             #if('mpi_procs' in params and params['mpi_procs'] > 1 and self.zerork_mpi_exe):
             #    np=str(params['mpi_procs'])
@@ -89,7 +91,7 @@ def zerork(dir_desk, atm, T0, fuel_fracs, oxid_fracs, phi, species_names, rxn_eq
             #                                       universal_newlines=True).split('\n')
             #else:
             zerork_out=subprocess.check_output([ZERORK_EXE,zerork_infile_name],
-                                                stderr=subprocess.STDOUT,universal_newlines=True).split('\n')
+                                                stderr=subprocess.STDOUT,universal_newlines=True, env=env).split('\n')
         except subprocess.CalledProcessError as e:
             zerork_out_file.write('!!! Warning: ZeroRK exited with non-zero output ({}).\n'.format(e.returncode))
             zerork_out=e.output.split('\n')
@@ -155,18 +157,17 @@ def zerork(dir_desk, atm, T0, fuel_fracs, oxid_fracs, phi, species_names, rxn_eq
             print("No data file from ZeroRK, ZeroRK output was:")
             for line in zerork_out:
                 print("\t", line)
-            sys.exit()
+            raise ValueError
 
         zerork_out_file.close()
 
     #Clean up
     finally:
         shutil.rmtree(tmpdir)
-        pass
 
     if(error_return or len(raw['axis0']) == 0):
         print(f"Zero-rk failed: {atm}, {T0}, {phi}")
-        sys.exit()
+        raise ValueError
 
     raw['net_reaction_rate'] = np.matrix(raw['net_reaction_rate']) * 1.0e3 #convert to mol/m^3/s
     raw['mole_fraction'] = np.matrix(raw['mole_fraction'])
